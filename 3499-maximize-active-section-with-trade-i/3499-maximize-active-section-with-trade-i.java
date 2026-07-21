bf0475f6fd6586f559c7f389fea903b2201c24a3
srijetas <srijetas@gmail.com>
2026-07-21 19:36:36 +0530

    Time: 68 ms (74.65%), Space: 47.9 MB (83.1%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int n = s.length();
+        int ans = 0, i = 0;
+        int pre = Integer.MIN_VALUE;
+        int mx = 0;
+
+        while (i < n) {
+            int j = i + 1;
+
+            while (j < n && s.charAt(j) == s.charAt(i)) {
+                j++;
+            }
+
+            int cur = j - i;
+
+            if (s.charAt(i) == '1') {
+                ans += cur;
+            } else {
+                mx = Math.max(mx, pre + cur);
+                pre = cur;
+            }
+
+            i = j;
+        }
+
+        return ans + mx;
+    }
+}
