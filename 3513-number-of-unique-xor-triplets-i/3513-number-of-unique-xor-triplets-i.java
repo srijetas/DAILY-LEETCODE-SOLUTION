9b35a9ca4a68f171cd4b0a4968e9b4072ae8fad7
Srijeta Sinha <srijetas@gmail.com>
2026-07-23 18:41:13 +0530

    Time: 0 ms (100%), Space: 125.4 MB (53.06%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        return Integer.highestOneBit(n) << 1;
+    }
+}
