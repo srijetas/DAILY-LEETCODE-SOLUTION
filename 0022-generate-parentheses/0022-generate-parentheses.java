eb6e968dd878d7676665a0a5c70326d42dce1706
Srijeta Sinha <srijetas@gmail.com>
2026-08-17 01:53:39 +0530

    Time: 8 ms (6.16%), Space: 45 MB (21.62%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack("", 0, 0, n, ans);
+        return ans;
+    }
+
+    void backtrack(String s, int open, int close, int n, List<String> ans) {
+
+        if (s.length() == 2 * n) {
+            ans.add(s);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            backtrack(s + "(", open + 1, close, n, ans);
+        }
+
+        // Add ')'
+        if (close < open) {
+            backtrack(s + ")", open, close + 1, n, ans);
+        }
+    }
+}
